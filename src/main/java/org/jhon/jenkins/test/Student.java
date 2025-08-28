0419ae659b2e1572390907a66c9cce5a0fe78fed
Jhon olivares <jhon.olivares@globant.com>
2025-08-28 09:55:37 -0500

    add Student class with properties and methods

---
+++ src/main/java/org/jhon/jenkins/test/Student.java
@@ -0,0 +1,40 @@
+package org.jhon.jenkins.test;
+
+public class Student {
+    private String name;
+    private String id;
+    private int age;
+
+    public Student(String name, String id, int age) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+}
